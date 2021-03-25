9d51d030f9ea05dd22bd6d219ac0f090a7d01a3b
kawai10 <77530278+kawai10@users.noreply.github.com>
2021-03-25 21:39:15 +0900

    star for loop

--- src/star.java
+++ src/star.java
@@ -1,11 +1,27 @@
 public class star {
     public static void main(String[] args) {
+        for(int i = 0; i < 10; i++) {
+            for (int j=0; j<10-i; j++) {
+                System.out.print("☆");
+            }
+            System.out.println();
+        }
 
-        for(int j = 0; j<10; j++) {
+
+
+        /*for(int i = 0; i < 10; i++) {
+            for (int j=0; j<i+1; j++) {
+                System.out.print("☆");
+            }
+            System.out.println();
+        }*/
+
+
+        /*for(int j = 0; j<10; j++) {
             for (int i = 0; i < 10; i++) {
                 System.out.printf("%c", '☆');
             }
             System.out.println();
-        }
+        }*/
     }
 }
