3788ba869c67336d40dd61ff8b2689c74ce49e03
kawai10 <77530278+kawai10@users.noreply.github.com>
2021-03-27 11:53:30 +0900

    Create Array2.java

---
+++ src/Array2.java
@@ -0,0 +1,19 @@
+public class Array2 {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3};
+        int[] nums2 = {4, 5, 6};
+        int[] nums = new int[6];
+
+        for(int i=0; i<3; i++)
+            nums[i] = nums1[i];
+
+        for(int i=0; i<6; i++)
+            System.out.println(nums[i]);
+
+        for(int i=0; i<3; i++)
+            nums[i+3] = nums2[i];
+
+        for(int i=0; i<6; i++)
+            System.out.println(nums[i]);
+    }
+}
