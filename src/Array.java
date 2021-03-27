227a768b89d60982885b132651ba3b365bdb12bc
kawai10 <77530278+kawai10@users.noreply.github.com>
2021-03-27 11:47:28 +0900

    Update Array.java

--- src/Array.java
+++ src/Array.java
@@ -1,19 +1,19 @@
 public class Array {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        for(int i=0; i<7; i++)
-            System.out.printf("%d ", nums[i]);
+        for(int i = 0; i<10; i++)
+            System.out.printf("%d ",nums[i]);
 
-        int[] temp = new int[10];
-
-        for(int i=0; i<7; i++)
+        int[] temp = new int[11];
+        for (int i =0; i<10; i++)
             temp[i] = nums[i];
 
         nums = temp;
-        nums[7] = 8;
 
-        for(int i=0; i<8; i++)
-            System.out.printf("%d ", nums[i]);
+        nums[10] = 11;
+        for(int i = 0; i<11; i++)
+            System.out.printf("%d ",nums[i]);
+
     }
 }
